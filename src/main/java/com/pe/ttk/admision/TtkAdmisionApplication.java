4fdbce087ea884568bc7cb84e248d249cf15ffd9
Jhan Karlo Santamaria Calderón <jhankarlo.smc@gmail.com>
2022-10-28 20:31:59 -0500

    Se agrega cors

--- src/main/java/com/pe/ttk/admision/TtkAdmisionApplication.java
+++ src/main/java/com/pe/ttk/admision/TtkAdmisionApplication.java
@@ -8,7 +8,9 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.TimeZone;
 
 @SpringBootApplication
@@ -23,7 +25,10 @@ public class TtkAdmisionApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("https://ttk-frontend.herokuapp.com/"));
+		List<String> sitios = new ArrayList<String>();
+		sitios.add("https://ttk-frontend.herokuapp.com/");
+		sitios.add("http://localhost:4200/");
+		corsConfiguration.setAllowedOrigins(sitios);
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
